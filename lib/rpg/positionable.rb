ac26abc50442650501b45772ce3215821e32999b
Josep M. Bach <josep.m.bach@gmail.com>
2012-02-18 20:10:04 +0100

    Add positionable mixin

---
+++ lib/rpg/positionable.rb
@@ -0,0 +1,23 @@
+module Rpg
+  module Positionable
+    attr_writer :x, :y
+
+    def x
+      @x || @parent.x
+    end
+
+    def y
+      @y || @parent.y
+    end
+
+    def pos(x, y)
+      @x, @y = x, y
+    end
+
+    Absolute = Object.new.tap do |obj|
+      obj.extend Positionable
+      obj.pos 0, 0
+    end
+  end
+
+end
---
+++ test/rpg/positionable_test.rb
@@ -0,0 +1,63 @@
+require 'test_helper'
+
+module Rpg
+  describe Positionable do
+    before do
+      @object = Class.new {
+        include Positionable
+        def initialize
+          @parent = Positionable::Absolute
+          @x = 100
+          @y = 200
+        end
+      }.new
+    end
+
+    describe '#x' do
+      describe 'if it has an x coordinate' do
+        it 'returns it' do
+          @object.x.must_equal 100
+        end
+      end
+      describe 'if not, but it has a parent' do
+        it "returns the parent's x" do
+          @object.x = nil
+          @object.x.must_equal 0
+        end
+      end
+    end
+
+    describe '#y' do
+      describe 'if it has an y coordinate' do
+        it 'returns it' do
+          @object.y.must_equal 200
+        end
+      end
+      describe 'if not, but it has a parent' do
+        it "returns the parent's y" do
+          @object.y = nil
+          @object.y.must_equal 0
+        end
+      end
+    end
+
+    describe '#pos' do
+      it 'sets x and y at once' do
+        @object.pos(1,2)
+        @object.x.must_equal 1
+        @object.y.must_equal 2
+      end
+    end
+
+    describe Positionable::Absolute do
+      before do
+        @absolute = Positionable::Absolute
+      end
+
+      it 'is a toplevel positionable object' do
+        @absolute.x.must_equal 0
+        @absolute.y.must_equal 0
+      end
+    end
+  end
+end
